a64a5c5f5e025ce32d5efe4d32d678ea2966d937
Karina <pste09.informatica@crecisp.gov.br>
2025-08-28 17:53:22 -0300

    Trabalhando layout HomeActivity: https://youtu.be/btdBRv-p34Q

--- app/src/main/java/com/example/calculadora/HomeActivity.java
+++ app/src/main/java/com/example/calculadora/HomeActivity.java
@@ -1,9 +1,6 @@
 package com.example.calculadora;
 
-import android.content.Intent;
 import android.os.Bundle;
-import android.widget.Button;
-import android.view.View;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -11,29 +8,17 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
-public class MainActivity extends AppCompatActivity {
+public class HomeActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_home);
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
-
-        Button btEntrar = findViewById(R.id.btEntrar);
-        btEntrar.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                // Abrir a tela home
-                Intent telaHome = new Intent(MainActivity.this, HomeActivity.class);
-                startActivity(telaHome);
-                finish();
-            }
-        });
-
     }
 }
